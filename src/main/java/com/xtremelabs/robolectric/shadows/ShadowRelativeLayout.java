413029d8b2c8134c96ff1db16657eceb3426c060
Kirwan Lyster <kirwan@zeebox.com>
2013-04-09 11:11:43 +0100

    Fixed RelativeLayout constructor that was not calling super

--- src/main/java/com/xtremelabs/robolectric/shadows/ShadowRelativeLayout.java
+++ src/main/java/com/xtremelabs/robolectric/shadows/ShadowRelativeLayout.java
@@ -4,17 +4,20 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
 import android.widget.RelativeLayout;
+
 import com.xtremelabs.robolectric.internal.Implements;
 
 @Implements(RelativeLayout.class)
 public class ShadowRelativeLayout extends ShadowViewGroup {
 
-    public void __constructor__(Context context) {
+	@Override
+	public void __constructor__(Context context) {
+		__constructor__(context, null, 0);
+	}
 
-    }
-
-    public void __constructor__(Context context, AttributeSet attributeSet, int defStyle) {
-        setLayoutParams(new ViewGroup.MarginLayoutParams(0, 0));
-        super.__constructor__(context, attributeSet, defStyle);
-    }
+	@Override
+	public void __constructor__(Context context, AttributeSet attributeSet, int defStyle) {
+		setLayoutParams(new ViewGroup.MarginLayoutParams(0, 0));
+		super.__constructor__(context, attributeSet, defStyle);
+	}
 }
